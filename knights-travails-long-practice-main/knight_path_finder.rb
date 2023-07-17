1b50ba1e7fb446ffeab5dbd271167f7449ce892b
ajb-4 <alexbrown4711@gmail.com>
2023-07-17 15:20:51 -0400

    KNights

--- knights-travails-long-practice-main/knight_path_finder.rb
+++ knights-travails-long-practice-main/knight_path_finder.rb
@@ -1,3 +1,4 @@
+require_relative "tree_node.rb"
 class KnightPathFinder
     def initialize(pos)
         @root_node = PolyTreeNode.new(pos)
@@ -6,4 +7,23 @@ class KnightPathFinder
 
     def build_move_tree
     end
+
+    def self.valid_moves(pos)
+    end
+
+
+    def valid_pos(pos)
+        row, col = pos
+        (row >= 0 && row < 8) && (col >= 0 && col < 8)
+    end
+    #avoid repeating positions
+
+
+    #BFS vs DFS
+
+    def find_path
+
+    end
+
+
 end
