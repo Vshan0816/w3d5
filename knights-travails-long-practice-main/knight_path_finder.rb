923ef9d2b1986a49352286b7e34c18b528769102
Vincent <vshan0816@gmail.com>
2023-07-17 15:38:37 -0400

    Finish valid moves

--- knights-travails-long-practice-main/knight_path_finder.rb
+++ knights-travails-long-practice-main/knight_path_finder.rb
@@ -3,16 +3,28 @@ class KnightPathFinder
     def initialize(pos)
         @root_node = PolyTreeNode.new(pos)
         self.build_move_tree
+        @considered_positions = []
     end
 
     def build_move_tree
     end
 
     def self.valid_moves(pos)
+        val_moves = []
+        row, col = pos
+        val_moves << [row+2, col+1] if valid_pos?([row+2, col+1])
+        val_moves << [row+2, col-1] if valid_pos?([row+2, col-1])
+        val_moves << [row-2, col+1] if valid_pos?([row-2, col+1])
+        val_moves << [row-2, col-1] if valid_pos?([row-2, col-1])
+        val_moves << [row+1, col+2] if valid_pos?([row+1, col+2])
+        val_moves << [row+1, col-2] if valid_pos?([row+1, col-2])
+        val_moves << [row-1, col+2] if valid_pos?([row-1, col+2])
+        val_moves << [row-1, col-2] if valid_pos?([row-1, col-2])
+        val_moves
     end
 
 
-    def valid_pos(pos)
+    def valid_pos?(pos)
         row, col = pos
         (row >= 0 && row < 8) && (col >= 0 && col < 8)
     end
